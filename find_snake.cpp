49247a7463a0d076d6abbc9ed3b474cc1c235d7e
brian731016 <brian731016@gmail.com>
2024-04-10 03:30:00 +0800

    with functions

--- find_snake.cpp
+++ find_snake.cpp
@@ -2,6 +2,50 @@
 #include<vector>
 #define N 60
 using namespace std;
+
+bool isSnake(int x,int y,vector<vector<char>> a,int n){
+    return (0<=x && x<n && 0<=y && y<n) && a[x][y]!='0';
+}
+
+void oneStep(int *x,int *y,int dir){
+	static vector<int> dx={1,0,0,-1};
+	static vector<int> dy={0,1,-1,0};
+	*x+=dx[dir];
+	*y+=dy[dir];
+}
+
+int tryOneStep(int *x,int *y,int oriDir,vector<vector<char>> a,int n,bool print){
+	static vector<int> dx={1,0,0,-1};
+	static vector<int> dy={0,1,-1,0};
+	if(print)cout<<a[*x][*y];
+	int newDir=-1;
+	for(int i=0;i<4 && newDir==-1;i++){
+		if(i==oriDir)continue;
+		if(isSnake(*x+dx[i],*y+dy[i],a,n)){
+			newDir=i;
+			oneStep(x,y,newDir);
+		}
+	}
+	return newDir;
+}
+
+void find(int *x,int *y,int oriDir,vector<vector<char>> a,int n,bool print){
+	static vector<int> dx={1,0,0,-1};
+	static vector<int> dy={0,1,-1,0};
+	int newDir=0;
+	while(newDir!=-1){
+		newDir=tryOneStep(x,y,oriDir,a,n,print);
+		oriDir=3-newDir;
+	}
+}
+
+void determineHead(int *hx,int *hy,int *tx,int *ty,vector<vector<char>> a){
+    if(a[*hx][*hy]-a[*tx][*ty]>0 || (a[*hx][*hy]-a[*tx][*ty]==0 && (*hx>*tx || (*hx==*tx && *hy>*ty)))){
+		swap(*hx,*tx);
+		swap(*hy,*ty);
+    }
+}
+
 int main(){
 	int n;
 	vector<vector<char>> a;
@@ -25,7 +69,7 @@ int main(){
 	vector<int> dirs={-1,-1};
 	int countDir=0;
 	for(int i=0;i<4;i++){
-		if(0<=x+dx[i] && x+dx[i]<n && 0<=y+dy[i] && y+dy[i]<n && a[x+dx[i]][y+dy[i]]!='0'){
+		if(isSnake(x+dx[i],y+dy[i],a,n)){
 			dirs[countDir++]=i;
 		}
 	}
@@ -34,73 +78,23 @@ int main(){
     if(countDir==1){	// the position we randomly chosed is either the head or the tail
         hx=x;
         hy=y;
-		int newDir=0;
-		dirs[0]=-1;
-		while(newDir!=-1){
-			newDir=-1;
-			for(int i=0;i<4 && newDir==-1;i++){
-				if(i==dirs[0])continue;
-				if(0<=x+dx[i] && x+dx[i]<n && 0<=y+dy[i] && y+dy[i]<n && a[x+dx[i]][y+dy[i]]!='0'){
-					newDir=i;
-					x+=dx[newDir];
-					y+=dy[newDir];
-					dirs[0]=3-newDir;
-				}
-			}
-		}
 		tx=x;
 		ty=y;
+		find(&tx,&ty,-1,a,n,false);
     }else{				// the position we randomly chosed is neither the head nor the tail
-        tx=x;
-        ty=y;
-		int newDir=0;
-		for(int j=0;j<2;j++){
-			newDir=0;
-			while(newDir!=-1){
-				newDir=-1;
-				for(int i=0;i<4 && newDir==-1;i++){
-					if(i==dirs[j])continue;
-					if(0<=x+dx[i] && x+dx[i]<n && 0<=y+dy[i] && y+dy[i]<n && a[x+dx[i]][y+dy[i]]!='0'){
-						newDir=i;
-						x+=dx[newDir];
-						y+=dy[newDir];
-						dirs[j]=3-newDir;
-					}
-				}
-			}
-			if(j==0){
-				hx=x;
-				hy=y;
-				x=tx;
-				y=ty;
-			}
-		}
+		hx=x;
+        hy=y;
+		find(&hx,&hy,dirs[0],a,n,false);
 		tx=x;
 		ty=y;
+		find(&tx,&ty,dirs[1],a,n,false);
     }
 	
 	// determine who is the head and who is the tail
-    if(a[hx][hy]-a[tx][ty]>0 || (a[hx][hy]-a[tx][ty]==0 && (hx>tx || (hx==tx && hy>ty)))){
-		swap(hx,tx);
-		swap(hy,ty);
-    }
+	determineHead(&hx,&hy,&tx,&ty,a);
 
 	// print the answer
-	dirs[0]=-1;
 	x=hx;
 	y=hy;
-	int newDir=0;
-	while(newDir!=-1){
-		newDir=-1;
-		cout<<a[x][y];
-		for(int i=0;i<4 && newDir==-1;i++){
-			if(i==dirs[0])continue;
-			if(0<=x+dx[i] && x+dx[i]<n && 0<=y+dy[i] && y+dy[i]<n && a[x+dx[i]][y+dy[i]]!='0'){
-				newDir=i;
-				x+=dx[newDir];
-				y+=dy[newDir];
-				dirs[0]=3-newDir;
-			}
-		}
-	}
+	find(&hx,&hy,-1,a,n,true);
 }
